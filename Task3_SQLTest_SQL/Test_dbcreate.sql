f3056919da92dde92b1faf7691dc69cfa60c2f0b
Andrey Vishin <vishin64@gmail.com>
2021-04-30 16:03:33 -0400

    Task3 after the terms of the task have been changed

--- Task3_SQLTest_SQL/Test_dbcreate.sql
+++ Task3_SQLTest_SQL/Test_dbcreate.sql
@@ -1,14 +1,17 @@
 CREATE TABLE Products
-    ([product_id] int, [product_type] varchar(7), [price] int)
+    ([product_id] int, [model] varchar(1), [product_type] varchar(7), [price] int)
 ;
     
 INSERT INTO Products
-    ([product_id], [product_type], [price])
+    ([product_id], [model], [product_type], [price])
 VALUES
-    (1, 'desktop', 1000),
-    (22, 'laptop', 999),
-    (3, 'laptop', 2999),
-    (14, 'laptop', 1999),
-    (5, 'laptop', 5000),
-    (36, 'desktop', 5000)
+    (1, 'D', 'desktop', 1000),
+    (22, 'G', 'laptop', 999),
+    (3, 'G', 'laptop', 2999),
+    (14, 'G', 'laptop', 1999),
+    (5, 'M', 'laptop', 5000),
+    (36, 'T', 'desktop', 5000),
+    (32, 'M', 'laptop', 2500),
+    (23, 'M', 'laptop', 1500),
+    (13, 'G', 'laptop', 1999)
 ;
--- Task3_SQLTest_SQL/Test_query.sql
+++ Task3_SQLTest_SQL/Test_query.sql
@@ -1,15 +1,5 @@
-SELECT product_id, price 
-FROM (
-  SELECT 
-  p.product_id, p.price,
-  AVG(p.price) OVER 
-    (ORDER BY p.price ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)
-                                                           AS average_price
-  FROM (
-    SELECT product_id, price
-    FROM Products
-    WHERE product_type='laptop'
-  ) p
-) t
-WHERE
-  average_price < 2000;
+SELECT model, AVG(price) as average_price
+FROM Products
+WHERE product_type='laptop'
+GROUP BY model
+HAVING AVG(price)<2000
